e74b2e510aa24f7bbf13e7c76ccaab4d1f8238ab
Ritik Mishra <ritik.mishra314@gmail.com>
2019-08-15 23:57:15 -0500

    Add support for sending message to slack by ifttt

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask, request
 import os
+import requests
+from email_parser import parse_email
 
 app = Flask(__name__)
+
+KEY = os.environ['IFTTT_GRABCAD_KEY']
+IFTTT_OUTBOUND_URL = f"https://maker.ifttt.com/trigger/grabcad_email_parsed/with/key/{KEY}"
 
 
 @app.route('/', methods=['POST'])
 def handle_grabcad_email():
-    print(request.data)
-    return '', 204
+    try:
+        slack_message = parse_email(request.data.decode("utf-8"))
+
+        requests.post(IFTTT_OUTBOUND_URL, json={'value1': slack_message})
+
+        return '', 204
+    except Exception as e:
+        return str(e), 500
+
 
 if __name__ == '__main__':
     debug = 'PRODUCTION' in os.environ.values()
@@ -15,6 +27,5 @@ if __name__ == '__main__':
         port = int(os.environ['PORT'])
     except KeyError:
         port = 5000
-
 
     app.run(debug=debug, port=port)
