e61a68a9a1d10604340def965d47dfd3c83c20ae
Ritik Mishra <ritik.mishra314@gmail.com>
2019-08-15 23:43:38 -0500

    figure out how to parse emails into a readable table

---
+++ email_parser.py
@@ -0,0 +1,86 @@
+import html5_parser
+from beautifultable import BeautifulTable
+
+# root = html5_parser.parse(
+#     """<div dir="ltr"><br><br><div class="gmail_quote"><div dir="ltr" class="gmail_attr">---------- Forwarded message ---------<br>From: <b class="gmail_sendername" dir="auto">Team 2502 Information</b> <span dir="auto">&lt;<a href="mailto:info@team2502.com">info@team2502.com</a>&gt;</span><br>Date: Thu, Aug 15, 2019 at 7:41 PM<br>Subject: Fwd: Ritik Mishra has left a project comment in FRC 2502 - Talon Robotics 2019 Offseason CAD<br>To:  &lt;<a href="mailto:trigger@applet.ifttt.com">trigger@applet.ifttt.com</a>&gt;<br></div><br><br><div dir="ltr"><br><br><div class="gmail_quote"><div dir="ltr" class="gmail_attr">---------- Forwarded message ---------<br>From: <b class="gmail_sendername" dir="auto">GrabCAD</b> <span dir="auto">&lt;<a href="mailto:members@grabcad.com" target="_blank">members@grabcad.com</a>&gt;</span><br>Date: Thu, Aug 15, 2019 at 7:23 PM<br>Subject: Ritik Mishra has left a project comment in FRC 2502 - Talon Robotics 2019 Offseason CAD<br>To:  &lt;<a href="mailto:info@team2502.com" target="_blank">info@team2502.com</a>&gt;<br></div><br><br>\n\n<div bgcolor="#5e5e5e" style="margin:0">\n<table width="100%" height="100%" cellspacing="0" cellpadding="0" bgcolor="#f3f3f3">\n\t<tbody><tr>\n\t\t<td valign="top" align="center">\n\t\t\t<table style="margin-top:20px;margin-bottom:8px" width="610px" cellspacing="0" cellpadding="0" border="0" bgcolor="#FFFFFF">\n\t\t\t\t<tbody><tr>\n\t\t\t\t\t<td style="height:55px;margin:0;padding:0;background-color:#000" valign="top" height="55px" bgcolor="#e6e6e6">\n\t\t\t\t\t\t<a href="https://grabcad.com/?utm_campaign=emailtemplate&amp;utm_content=headerlogom&amp;utm_medium=email&amp;utm_source=user-email" style="border-style:none;margin:0;padding:0;display:block" target="_blank">\n\t\t\t\t\t\t\t<img src="http://workbench.grabcad.com/static/mails/grab_logo.png" alt="GrabCAD" style="border-style:none;margin-top:8px" height="39">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t\t<tr>\n\t\t\t\t\t<td style="font-family:Arial,Helvetica,sans-serif;font-size:16px;color:#666666;padding:30px">\n\t\t\t\t\t\t\n\t\t\t\t\t\t<table width="540px" cellspacing="0" cellpadding="0">\n\t<tbody><tr>\n\t\t<td style="font-size:18px;color:#000;font-family:Arial,Helvetica,sans-serif" valign="bottom" height="50px">\n\t\t\t<a href="https://grabcad.com/ritik.mishra-2?utm_campaign=newcomment&amp;utm_content=heading&amp;utm_medium=email&amp;utm_source=member-email" style="text-decoration:underline;color:#cc0606;font-weight:bold" target="_blank">Ritik Mishra</a> left a comment on <b></b><a href="https://workbench.grabcad.com/workbench/projects/gcVPoIWVsn9dcKtTwnt1cVfHjg9ez24smbzvBe4SxICjfi?event=added&amp;project=944505&amp;recipient=project&amp;recipient_id=944505&amp;result=comment&amp;result_id=1673064&amp;utm_campaign=newcomment&amp;utm_content=heading&amp;utm_medium=email&amp;utm_source=member-email" style="text-decoration:underline;color:#cc0606;font-weight:bold" target="_blank">FRC 2502 - Talon Robotics 2019 Offseason CAD</a>\n\t\t</td>\n\t</tr>\n\t<tr>\n\t\t<td>\n\t\t\t\t<table style="padding-top:8px" cellspacing="0" cellpadding="0">\n\t\t\t\t\t<tbody><tr>\n\t\t\t\t\t\t<td style="color:#333;line-height:21px">\n\t\t\t\t\t\t\t<p>(test project update) things are going well though\n</p>\n\t\t\t\t\t\t</td>\n\t\t\t\t\t</tr>\n\t\t\t\t\t<tr>\n\t\t\t\t\t\t<td style="padding-top:20px">\n\t\t\t\t\t\t\t<a href="https://workbench.grabcad.com/workbench/projects/gcVPoIWVsn9dcKtTwnt1cVfHjg9ez24smbzvBe4SxICjfi?event=added&amp;project=944505&amp;recipient=project&amp;recipient_id=944505&amp;result=comment&amp;result_id=1673064&amp;utm_campaign=newcomment&amp;utm_content=joinconversation&amp;utm_medium=email&amp;utm_source=member-email" style="border:1px solid #b60000;color:#ffffff;display:inline-block;font-family:Arial,Helvetica,sans-serif;font-size:15px;font-weight:normal;font-style:normal;line-height:28px;padding:0px 13px;text-decoration:none;vertical-align:baseline;background-color:#ef3030" align="center" target="_blank">\n\t\t\t\t\t\t\t\tReply to comment\n\t\t\t\t\t\t\t</a>\n                                <span style="color:#999;font-size:15px;line-height:21px;padding-left:5px">You can also reply to this email directly to post a response.</span>\n\t\t\t\t\t\t</td>\n\t\t\t\t\t</tr>\n\t\t\t\t</tbody></table>\n\t\t</td>\n\t</tr>\n</tbody></table>\n\t\t\t\t\t\t\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t</tbody></table>\n\t\t\t<table width="600px">\n\t\t\t\t<tbody><tr>\n\t\t\t\t\t<td style="font-size:12px;color:#999;font-family:Arial,Helvetica,sans-serif;text-align:left" align="center">\n\t\t\t\t\t\t<a href="http://facebook.com/grabcad" style="border-style:none;text-decoration:none" target="_blank">\n\t\t\t\t\t\t\t<img src="http://workbench.grabcad.com/static/mails/facebook.png" alt="Facebook" style="border-style:none" width="32" height="32">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t\xc2\xa0\n\t\t\t\t\t\t<a href="http://twitter.com/grabcad" style="border-style:none;text-decoration:none" target="_blank">\n\t\t\t\t\t\t\t<img src="http://workbench.grabcad.com/static/mails/twitter.png" alt="Twitter" style="border-style:none" width="32" height="32">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t\t<tr>\n\t\t\t\t\t<td style="font-size:11px;color:#aaa;font-family:Arial,Helvetica,sans-serif;text-align:left;line-height:15px;padding-top:8px;height:80px" valign="top" align="center">\n\t\t\t\t\t\t\n\t\t\t\t\t\t<a style="color:#ccc" href="https://grabcad.com/email/unsubscribe?email=info%40team2502.com&amp;token=dcd26d1e179c4fca4ae7a83eb4e26759" target="_blank">Unsubscribe</a>\n\t\t\t\t\t\tfrom all GrabCAD e-mails or\n\t\t\t\t\t\t<a style="color:#ccc" href="https://grabcad.com/community/email/edit" target="_blank">manage</a>\n\t\t\t\t\t\temail preferences.\n\t\t\t\t\t\t<br>\n\t\t\t\t\t\t<br>\n\t\t\t\t\t\t<br>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t</tbody></table>\n\t\t</td>\n\t</tr>\n</tbody></table>\n</div>\n\n</div></div>\n</div></div>""")
+
+root = html5_parser.parse(
+    """
+     <div dir="ltr"><br><br><div class="gmail_quote"><div dir="ltr" class="gmail_attr">---------- Forwarded message ---------<br>From: <b class="gmail_sendername" dir="auto">GrabCAD</b> <span dir="auto">&lt;<a href="mailto:members@grabcad.com">members@grabcad.com</a>&gt;</span><br>Date: Thu, Aug 15, 2019 at 7:25 PM<br>Subject: Ritik Mishra has updated files in FRC 2502 - Talon Robotics 2019 Offseason CAD<br>To:  &lt;<a href="mailto:info@team2502.com">info@team2502.com</a>&gt;<br></div><br><br>\n\n<div bgcolor="#5e5e5e" style="margin:0">\n<table width="100%" height="100%" cellspacing="0" cellpadding="0" bgcolor="#f3f3f3">\n\t<tbody><tr>\n\t\t<td valign="top" align="center">\n\t\t\t<table style="margin-top:20px;margin-bottom:8px" width="610px" cellspacing="0" cellpadding="0" border="0" bgcolor="#FFFFFF">\n\t\t\t\t<tbody><tr>\n\t\t\t\t\t<td style="height:55px;margin:0;padding:0;background-color:#000" valign="top" height="55px" bgcolor="#e6e6e6">\n\t\t\t\t\t\t<a href="https://grabcad.com/?utm_campaign=emailtemplate&amp;utm_content=headerlogom&amp;utm_medium=email&amp;utm_source=user-email" style="border-style:none;margin:0;padding:0;display:block" target="_blank">\n\t\t\t\t\t\t\t<img src="http://workbench.grabcad.com/static/mails/grab_logo.png" alt="GrabCAD" style="border-style:none;margin-top:8px" height="39">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t\t<tr>\n\t\t\t\t\t<td style="font-family:Arial,Helvetica,sans-serif;font-size:16px;color:#666666;padding:30px">\n\t\t\t\t\t\t\n\t\t\t\t\t\t<table width="540px" cellspacing="0" cellpadding="0">\n\t<tbody><tr>\n\t\t<td>\n\t\t\t<table>\n\t\t\t\t<tbody><tr>\n\t\t\t\t\t<td>\n\t\t\t\t\t\t<a href="https://grabcad.com/ritik.mishra-2?utm_campaign=eng-snapshot_created&amp;utm_content=snapshot-created&amp;utm_medium=cadfile&amp;utm_source=user-email" style="text-decoration:underline;color:#cc0606" target="_blank">Ritik Mishra</a>\n\t\t\t\t\t\t has updated files in \n\t\t\t\t\t\t\t<a href="https://workbench.grabcad.com/workbench/projects/gcVPoIWVsn9dcKtTwnt1cVfHjg9ez24smbzvBe4SxICjfi?event=added&amp;project=944505&amp;recipient=project&amp;recipient_id=944505&amp;result=snapshot&amp;result_id=5428529&amp;utm_campaign=eng-snapshot_created&amp;utm_content=see_cadfile&amp;utm_medium=cadfile&amp;utm_source=user-email" style="text-decoration:underline;color:#cc0606" target="_blank">\n\t\t\t\t\t\t\t\tFRC 2502 - Talon Robotics 2019 Offseason CAD</a>.\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t\t\t<tr>\n\t\t\t\t\t\t<td style="padding:10px 0 0 0">\n\t\t\t\t\t\t\tDeleted 1 file and 0 folders\n\t\t\t\t\t\t</td>\n\t\t\t\t\t</tr>\n\t\t\t\t<tr>\n\t\t\t\t\t<td style="font-size:12px;padding:10px 0 2px 0">\n\t\t\t\t\t\tFiles changed:\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t\t<tr>\n\t\t\t\t\t<td>\n\t\t\t\t\t\t<table style="border:1px solid #eee;font-size:11px" width="540" cellspacing="0" cellpadding="0">\n\t\t\t\t\t\t\t<tbody><tr>\n\t\t\t\t\t\t        <td style="color:#aaa;padding:3px" bgcolor="#eee">\n\t\t\t\t\t\t        \tAction\n\t\t\t\t\t\t        </td>\n\t\t\t\t\t\t        <td style="color:#aaa;padding:3px 0" bgcolor="#eee">\n\t\t\t\t\t\t        \tName\n\t\t\t\t\t\t        </td>\n\t\t\t\t\t\t    </tr>\n\t\t\t\t\t\t\t\t\t<tr>\n\t\t\t\t\t\t\t\t\t\t<td style="padding-right:10px;width:140px">\n\t\t\t\t\t\t\t\t\t\t\t<span style="height:24px;width:24px;float:left;background:url(http://workbench.grabcad.com/images/_/icon-minus-12x12.png) no-repeat center"></span>\n\t\t\t\t\t\t\t\t\t\t\t<span style="float:left;line-height:24px">Removed</span>\n\t\t\t\t\t\t\t\t\t\t</td>\n\t\t\t\t\t\t\t\t\t\t<td>\n\t\t\t\t\t\t\t\t\t\t\t\t<b>XX - Prototypes/Hatch/Gators Grabber/assembled_grabber_polished.avi</b>\n\t\t\t\t\t\t\t\t\t\t</td>\n\t\t\t\t\t\t\t\t\t</tr>\n\t\t\t\t\t\t</tbody></table>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t\t<tr>\n\t\t\t\t\t<td style="padding:25px 0 5px 0">\n\t\t\t\t\t\t\t\t<a href="https://workbench.grabcad.com/workbench/projects/gcVPoIWVsn9dcKtTwnt1cVfHjg9ez24smbzvBe4SxICjfi?event=added&amp;project=944505&amp;recipient=project&amp;recipient_id=944505&amp;result=snapshot&amp;result_id=5428529&amp;utm_campaign=eng-snapshot_created&amp;utm_content=see_cadfile&amp;utm_medium=cadfile&amp;utm_source=user-email#/event/62686280" style="border:1px solid #b60000;color:#ffffff;display:inline-block;font-family:Arial,Helvetica,sans-serif;font-size:15px;font-weight:normal;font-style:normal;line-height:28px;padding:0px 13px;text-decoration:none;vertical-align:baseline;background-color:#ef3030" target="_blank">\n\t\t\t\t\t\t\t\t\tView changes\n\t\t\t\t\t\t\t\t</a>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t</tbody></table>\n\t\t</td>\n\t</tr>\n</tbody></table>\n\n\n\t\t\t\t\t\t\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t</tbody></table>\n\t\t\t<table width="600px">\n\t\t\t\t<tbody><tr>\n\t\t\t\t\t<td style="font-size:12px;color:#999;font-family:Arial,Helvetica,sans-serif;text-align:left" align="center">\n\t\t\t\t\t\t<a href="http://facebook.com/grabcad" style="border-style:none;text-decoration:none" target="_blank">\n\t\t\t\t\t\t\t<img src="http://workbench.grabcad.com/static/mails/facebook.png" alt="Facebook" style="border-style:none" width="32" height="32">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t\xc2\xa0\n\t\t\t\t\t\t<a href="http://twitter.com/grabcad" style="border-style:none;text-decoration:none" target="_blank">\n\t\t\t\t\t\t\t<img src="http://workbench.grabcad.com/static/mails/twitter.png" alt="Twitter" style="border-style:none" width="32" height="32">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t\t<tr>\n\t\t\t\t\t<td style="font-size:11px;color:#aaa;font-family:Arial,Helvetica,sans-serif;text-align:left;line-height:15px;padding-top:8px;height:80px" valign="top" align="center">\n\t\t\t\t\t\t\n\t\t\t\t\t\t<a style="color:#ccc" href="https://grabcad.com/email/unsubscribe?email=info%40team2502.com&amp;token=dcd26d1e179c4fca4ae7a83eb4e26759" target="_blank">Unsubscribe</a>\n\t\t\t\t\t\tfrom all GrabCAD e-mails or\n\t\t\t\t\t\t<a style="color:#ccc" href="https://grabcad.com/community/email/edit" target="_blank">manage</a>\n\t\t\t\t\t\temail preferences.\n\t\t\t\t\t\t<br>\n\t\t\t\t\t\t<br>\n\t\t\t\t\t\t<br>\n\t\t\t\t\t</td>\n\t\t\t\t</tr>\n\t\t\t</tbody></table>\n\t\t</td>\n\t</tr>\n</tbody></table>\n</div>\n\n</div></div>\n
+    """
+)
+body = None
+
+for child in root:
+    if child.tag == 'body':
+        body = child
+        break
+else:
+    raise Exception("body not found??")
+
+outermost_table = None
+
+
+def look_for_outermost_tag(root, tag_name, initial_caller=True):
+    if root.tag == tag_name and not initial_caller:
+        return root
+    if len(root) == 0:
+        return None
+    else:
+        for child in root:
+            maybe_table = look_for_outermost_tag(child, tag_name, False)
+            if maybe_table is not None:
+                return maybe_table
+        else:
+            return None
+
+def flatten_text_in_td(root):
+    text = ""
+    if root.text is not None:
+        text += root.text + " "
+
+    if root.tag == 'tbody':
+        return table_to_beautifultable(root)
+
+    for child in root:
+        child_text = flatten_text_in_td(child)
+        if type(child_text) == str:
+            text += child_text
+        elif type(child_text) == BeautifulTable:
+            return child_text
+        else:
+            raise Exception(type(child_text))
+
+    if root.tail is not None:
+        text += root.tail + " "
+    return text.strip().replace("\t", "")
+
+def table_to_beautifultable(root):
+    assert root.tag == 'tbody'
+
+    bt_table = BeautifulTable()
+
+    for child in root:
+        assert child.tag == 'tr'
+        row = []
+        for data in child:
+            assert data.tag == 'td'
+            row.append(flatten_text_in_td(data))
+
+
+        bt_table.append_row(row)
+
+    return bt_table
+
+
+
+table = look_for_outermost_tag(body, 'td')
+
+email_content_tbody = table[0][0][1][0][0][0]
+
+action_tree = email_content_tbody[0][0]
+
+action_title = action_tree[0]
+
+
+print(table_to_beautifultable(email_content_tbody))
